2e32c4405ab80151202edf2d76cbff20681dced2
sanjanavurity <128248037+sanjanavurity@users.noreply.github.com>
2025-02-14 11:57:29 +0530

    Add files via upload

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,32 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
+
+namespace WebApplication18.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult SendEmail(string body)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("mose95@ethereal.email"));
+            email.To.Add(MailboxAddress.Parse("mose95@ethereal.email"));
+            email.Subject = "Test Email Subject";
+            email.Body = new TextPart(TextFormat.Html) { Text = body };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate("mose95@ethereal.email", "SMSGVGhy7NsR6RUHsB");
+            smtp.Send(email);
+            smtp.Disconnect(true);
+
+            return Ok();
+        }
+    }
+}
