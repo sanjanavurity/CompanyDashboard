f4540ee2bc4081dd865a1b549369559d9fa61e4a
sanjanavurity <128248037+sanjanavurity@users.noreply.github.com>
2025-02-14 11:59:03 +0530

    Add files via upload

---
+++ Services/EmailService/EmailService.cs
@@ -0,0 +1,26 @@
+﻿using MailKit.Security;
+using MimeKit.Text;
+using MimeKit;
+using MailKit.Net.Smtp;
+using WebApplication18.Models;
+
+namespace WebApplication18.Services.EmailService
+{
+    public class EmailService : IEmailService
+    {
+        public void SendEmail(EmailDto request)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("mose95@ethereal.email"));
+            email.To.Add(MailboxAddress.Parse(request.To));
+            email.Subject = request.Subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = request.Body };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate("mose95@ethereal.email", "SMSGVGhy7NsR6RUHsB");
+            smtp.Send(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
---
+++ Services/EmailService/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using WebApplication18.Models;
+
+namespace WebApplication18.Services.EmailService
+{
+    public interface IEmailService
+    {
+        void SendEmail(EmailDto request);
+    }
+}
